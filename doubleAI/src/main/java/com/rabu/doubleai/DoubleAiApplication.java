dfaf5dbee0b2d6e436100529a9f3e3ef97956757
kimmin930 <kimmin930@naver.com>
2025-01-31 20:33:58 +0900

    init
    springboot

---
+++ doubleAI/src/main/java/com/rabu/doubleai/DoubleAiApplication.java
@@ -0,0 +1,13 @@
+package com.rabu.doubleai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleAiApplication.class, args);
+	}
+
+}
---
+++ doubleAI/src/test/java/com/rabu/doubleai/DoubleAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabu.doubleai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
